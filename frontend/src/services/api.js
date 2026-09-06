f89e5cbc83bd7e8659a6e59f26ee19a4281a95e5
kabilanssj-coder <kabilanssj@gmail.com>
2026-09-06 09:52:02 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL =  "https://cars-pack-1.onrender.com/api";
+const API_BASE_URL = "https://cars-pack.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
